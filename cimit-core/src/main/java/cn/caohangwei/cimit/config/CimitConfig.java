a6246bee4f2f72462d1dba0cdf4409c39cc5f5bb
PinuoC <104079381@qq.com>
2019-07-18 16:58:41 +0800

    add cimit config file

---
+++ cimit-core/src/main/java/cn/caohangwei/cimit/config/CimitConfig.java
@@ -0,0 +1,57 @@
+package cn.caohangwei.cimit.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+
+public class CimitConfig {
+
+    private static final String PREFIX = "classpath:";
+
+    private static final String DEFAULT_CIMIT_CONFIG_PATH = "classpath:cimit.properties";
+
+    private static Properties properties = new Properties();
+
+    static {
+        load();
+    }
+
+    private static void load(){
+        String filename = DEFAULT_CIMIT_CONFIG_PATH;
+        loadProperties(filename);
+    }
+
+    private static void loadProperties(String filename){
+        filename = filename.substring(PREFIX.length());
+        List<URL> list = new ArrayList<>();
+        try {
+            Enumeration<URL> urls = CimitConfig.class.getClassLoader().getResources(filename);
+            while(urls.hasMoreElements()){
+                list.add(urls.nextElement());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(list.isEmpty()){
+            return ;
+        }
+        for(URL url : list){
+            Properties p = new Properties();
+            try {
+                InputStream in = url.openStream();
+                p.load(in);
+                properties.putAll(p);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Properties getProperties() {
+        return properties;
+    }
+}
