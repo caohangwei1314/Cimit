9f78309fb6e051694f423e8c04b5fc6f4949c49c
PinuoC <104079381@qq.com>
2019-07-19 16:11:49 +0800

    add http util class

---
+++ cimit-core/src/main/java/cn/caohangwei/cimit/util/HttpUtil.java
@@ -0,0 +1,31 @@
+package cn.caohangwei.cimit.util;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+
+public class HttpUtil {
+
+    private static volatile CloseableHttpClient httpClient = HttpClients.createDefault();
+
+    public static String sendPost(String url, String jsonStr) throws IOException {
+        String result;
+        StringEntity stringEntity = new StringEntity(jsonStr);
+        stringEntity.setContentType("application/json");
+        HttpPost httpPost = new HttpPost("http://" + url);
+        httpPost.setEntity(stringEntity);
+        try (CloseableHttpResponse httpResponse = httpClient.execute(httpPost);) {
+            HttpEntity httpEntity = httpResponse.getEntity();
+            result = EntityUtils.toString(httpEntity);
+        }
+        return result;
+    }
+
+}
