4f0a4760306358dc49d69841a3ff84338131ee94
PinuoC <104079381@qq.com>
2019-07-19 16:32:17 +0800

    add cimit-server test unit

--- cimit-core/src/test/java/cn/caohangwei/cimit/core/LimiterFactoryTest.java
+++ cimit-core/src/test/java/cn/caohangwei/cimit/core/LimiterFactoryTest.java
@@ -1,6 +1,11 @@
 package cn.caohangwei.cimit.core;
 
+import cn.caohangwei.cimit.common.LimiterRule;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +17,25 @@ class LimiterFactoryTest {
         for (int i = 0; i < 20; i++) {
             new Thread(() -> {
                 if (limiter.tryAcquire()) {
+                    System.out.println("Hello Cimit!");
+                }
+            }).start();
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void distributed(){
+        LimiterRule rule = new LimiterRule("distributed",10,10,1, TimeUnit.SECONDS,true);
+        AbstractLimiter limiter = LimiterFactory.getLeakyBucketLimiter(rule);
+        limiter.tryAcquire();
+        for(int i=0;i<20;i++){
+            new Thread(()->{
+                if(limiter.tryAcquire()){
                     System.out.println("Hello Cimit!");
                 }
             }).start();
---
+++ cimit-server/src/test/java/cn/caohangwei/cimit/server/CimitServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.caohangwei.cimit.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CimitServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
